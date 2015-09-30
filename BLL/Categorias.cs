9abb30e46999810c77e54f573ca0058ea1296917
enelramon@gmail.com <enelramon@gmail.com>
2015-09-29 21:08:45 -0400

    1. Agregand proyecto DAL 2. Agregando Proyecto BLL 3. Agregando las referencias entre los proyectos 4. Agregamos la clase categoria solo con el metodo insertar.

---
+++ BLL/Categorias.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL;
+
+namespace BLL
+{
+    public class Categorias
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public Categorias()
+        {
+            this.Id = 0;
+            this.Descripcion = "";
+        }
+
+        public Categorias(int id, string descripcion)
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+        }
+
+
+        private bool Insertar()
+        {
+            bool retorno = false;
+
+            ConexionDb conexion = new ConexionDb();
+
+            conexion.Ejecutar(String.Format("Insert Into Categorias (Descripcion) Values('{0}')", this.Descripcion));
+
+            return retorno;
+
+        }
+
+    }
+}
